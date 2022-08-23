af54ea08f2ed8ef5a2a5afcc67225c142dd8701e
TheMalachite <eliasgheeraert@gmail.com>
2022-08-23 19:48:59 +0200

    sm8250-common: Add Hotword Enrollment blobs

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,9 @@ function blob_fixup() {
         odm/lib64/libgf_hal_G3.so)
             sed -i "s|ro.boot.flash.locked|vendor.flash.locked\x00|" "${2}"
             ;;
+        product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml)
+            sed -i "s|my_product|product|" "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "AB 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
